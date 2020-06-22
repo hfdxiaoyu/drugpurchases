029885945951f8a59dc00095ea24c68c8ea3bbb3
hfdxiaoyu <1306933520@qq.com>
2020-06-22 11:12:11 +0800

    创建好所有实体类1

--- src/main/java/com/drug/po/Medicin.java
+++ src/main/java/com/drug/po/Medicin.java
@@ -4,7 +4,77 @@ package com.drug.po;
  * @author 13069
  *
  */
+
 public class Medicin {
 	private int medid;//药品编号
+	private String medname;//药品名字
+	private String medspe;//药品剂量
+	private String medprince;//药品单价
+	private String medpro;//药品生产企业
+	private String medsdata;//采购时间
+	private int medval;//有效期
+	private Person persons;//关联人员表
+	private Purchase purchases;//关联采购表
+	public int getMedid() {
+		return medid;
+	}
+	public void setMedid(int medid) {
+		this.medid = medid;
+	}
+	public String getMedname() {
+		return medname;
+	}
+	public void setMedname(String medname) {
+		this.medname = medname;
+	}
+	public String getMedspe() {
+		return medspe;
+	}
+	public void setMedspe(String medspe) {
+		this.medspe = medspe;
+	}
+	public String getMedprince() {
+		return medprince;
+	}
+	public void setMedprince(String medprince) {
+		this.medprince = medprince;
+	}
+	public String getMedpro() {
+		return medpro;
+	}
+	public void setMedpro(String medpro) {
+		this.medpro = medpro;
+	}
+	public String getMedsdata() {
+		return medsdata;
+	}
+	public void setMedsdata(String medsdata) {
+		this.medsdata = medsdata;
+	}
+	public int getMedval() {
+		return medval;
+	}
+	public void setMedval(int medval) {
+		this.medval = medval;
+	}
+	public Person getPersons() {
+		return persons;
+	}
+	public void setPersons(Person persons) {
+		this.persons = persons;
+	}
+	public Purchase getPurchases() {
+		return purchases;
+	}
+	public void setPurchases(Purchase purchases) {
+		this.purchases = purchases;
+	}
+	@Override
+	public String toString() {
+		return "Medicin [medid=" + medid + ", medname=" + medname + ", medspe=" + medspe + ", medprince=" + medprince
+				+ ", medpro=" + medpro + ", medsdata=" + medsdata + ", medval=" + medval + ", persons=" + persons
+				+ ", purchases=" + purchases + "]";
+	}
+	
 	
 }
